1c1a4ab9fac3534101494e6d60a19f3a0e723d45
Paloma Kimberly <57427551+Novetres@users.noreply.github.com>
2022-05-15 02:17:29 +1100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,14 +1,7 @@
-/* var btn = document.querySelector("#tipos");
-btn.addEventListener("click", function() {
-    window.scrollTo(0, 600);
-}); */
+
 
 var btn = document.querySelector("#topo");
 btn.addEventListener("click", function() {
     window.scrollTo(0, 0);
 });
 
-/* var btn = document.querySelector("#faleconosco");
-btn.addEventListener("click", function() {
-    window.scrollTo(0, 1999);
-}); */
