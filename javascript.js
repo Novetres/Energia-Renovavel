b02e23471cacaa1e4ca5c6098d33933836f8b948
Paloma Kimberly <57427551+Novetres@users.noreply.github.com>
2022-05-25 02:32:13 +1100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -5,3 +5,8 @@ btn.addEventListener("click", function() {
     window.scrollTo(0, 0);
 });
 
+function bemvindo()
+{
+alert("Antes de prosseguir, certifique-se que no ambiente em que você está não há nenhum tipo de desperdício de energia, a natureza agradece!");
+}
+
